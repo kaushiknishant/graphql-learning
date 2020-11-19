e70bb3276ff539457810070cffbe6bbf28caf176
nishantsharma12 <nishant@navigus.in>
2020-11-19 09:33:39 +0530

    setup server file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Start your es6 scripts here
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Up and running with graphql");
+});
+
+app.listen(8082, () => {
+  console.log("Running at 8082");
+});
